9900ae7b14ee5750618ef585dd6ecd8ca18ec9b2
ApexRecording <guaproyal@gmail.com>
2025-12-18 20:58:54 -0500

    fix: Simplify SOPWrapper to fix HMR Suspense error
    Removed nested Suspense boundary in SOPWrapper that was causing
    React Fast Refresh errors during HMR. The route-level Suspense
    boundary in App.tsx is sufficient.
    
    Double Suspense boundaries + non-lazy component was confusing React's
    HMR system and causing 'component suspended during synchronous input'
    errors on hot reload.
    
    Result: Clean component hierarchy, proper HMR support

--- src/portals/company/sop/SOPWrapper.tsx
+++ src/portals/company/sop/SOPWrapper.tsx
@@ -1,33 +1,9 @@
-import React, { Suspense } from 'react';
-import { Center, Loader, Container } from '@mantine/core';
+import React from 'react';
 import SOPManagement from './SOPManagement';
 
-console.log('🔷 [SOP] SOPWrapper module loaded');
-
-const LoadingFallback = () => (
-  <Container size="xl" py="xl">
-    <Center h={400}>
-      <Loader size="lg" />
-    </Center>
-  </Container>
-);
-
 const SOPWrapper: React.FC = () => {
-  console.log('🔷 [SOP] SOPWrapper component function called!');
-  console.log('🔷 [SOP] About to render SOPManagement...');
-  
-  try {
-    return (
-      <Suspense fallback={<LoadingFallback />}>
-        <SOPManagement />
-      </Suspense>
-    );
-  } catch (error) {
-    console.error('🔷 [SOP] Error rendering SOPManagement:', error);
-    return <div>Error loading SOP Management</div>;
-  }
+  return <SOPManagement />;
 };
 
-console.log('🔷 [SOP] SOPWrapper component defined, exporting...');
 export default SOPWrapper;
 
